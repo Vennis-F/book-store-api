cdcf9ced94e3a1093f4ea56887d8d2d14d3ca9c1
vennis <hoanganhgo28062001@gmail.com>
2022-06-22 09:55:35 +0700

    code v2

--- src/routers/products.js
+++ src/routers/products.js
@@ -46,6 +46,37 @@ router.get("/", async (req, res) => {
   }
 });
 
+//GET /products/category/
+router.get("/category/:categoryId", async (req, res) => {
+  const cateId = req.params.categoryId;
+  const { limit, page, sortBy, publicDate } = req.query;
+  const match = { category: cateId };
+  const sort = { "briefInformation.publicDate": -1 };
+  const options = {
+    limit: 12,
+    skip: 0,
+    sort,
+  };
+
+  //Paging
+  if (limit) options.limit = parseInt(limit);
+  if (page) options.skip = parseInt(limit) * (parseInt(skip) - 1);
+  if (publicDate) {
+    sort["briefInformation.publicDate"] = publicDate === "desc" ? -1 : 1;
+  }
+  console.log(match);
+
+  try {
+    //Find and Check product exist:
+    const product = await Product.find(match, null, options);
+    res.send(product);
+  } catch (e) {
+    if (e.name === "CastError" && e.kind === "ObjectId")
+      return res.status(400).send({ error: "Invalid ID" });
+    res.status(500).send(e);
+  }
+});
+
 //GET /products/:id
 router.get("/:id", async (req, res) => {
   try {
@@ -61,27 +92,25 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-//GET /products/category/
+// //GET /products/category/:id
+// router.get("/category/:id", async (req, res) => {
+//   const cateId = req.body.categoryId;
 
-//GET /products/category/:id
-router.get("/category/:id", async (req, res) => {
-  const cateId = req.body.categoryId;
+//   try {
+//     //Find and Check product exist:
+//     const product = await Product.findOne({
+//       _id: req.params.id,
+//       category: cateId,
+//     });
+//     if (!product) return res.sendStatus(404);
 
-  try {
-    //Find and Check product exist:
-    const product = await Product.findOne({
-      _id: req.params.id,
-      category: cateId,
-    });
-    if (!product) return res.sendStatus(404);
-
-    res.send(product);
-  } catch (e) {
-    if (e.name === "CastError" && e.kind === "ObjectId")
-      return res.status(400).send({ error: "Invalid ID" });
-    res.status(500).send(e);
-  }
-});
+//     res.send(product);
+//   } catch (e) {
+//     if (e.name === "CastError" && e.kind === "ObjectId")
+//       return res.status(400).send({ error: "Invalid ID" });
+//     res.status(500).send(e);
+//   }
+// });
 
 //PATCH /categories/:id (ALL field)
 router.patch("/:id", auth, authorize("marketing"), async (req, res) => {
