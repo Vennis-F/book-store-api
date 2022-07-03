e7d6bf05f8247dbb9f857a05987285ffcae364fb
vennis <hoanganhgo28062001@gmail.com>
2022-07-03 21:15:41 +0700

    demo2 first

--- src/routers/slider.js
+++ src/routers/slider.js
@@ -22,9 +22,28 @@ router.post("/", async (req, res) => {
 
 //GET /sliders
 router.get("/", async (req, res) => {
+  console.log(req.query);
+
+  const { limit, page, publicDate, status } = req.query;
+  const match = {};
+  const options = {
+    limit: 10,
+    skip: 0,
+  };
+
+  //Product status
+  if (status) {
+    match.status = status === "true";
+  }
+
+  //Paging
+  if (limit) options.limit = parseInt(limit);
+  if (page) options.skip = parseInt(limit) * (parseInt(page) - 1);
+
   try {
-    const sliders = await Slider.find({});
+    const sliders = await Slider.find(match, null, options);
     const count = await Slider.countDocuments();
+    // console.log({ sliders, count });
     res.status(200).send({ sliders, count });
   } catch (error) {
     console.log(error);
@@ -70,3 +89,28 @@ router.patch("/:id", async (req, res) => {
 });
 
 module.exports = router;
+
+//SECTION 1:
+//Q1: 93
+let total = 0;
+for (let i = 3; i <= 30; i += 3) {
+  total = total + i;
+  i = i + 2;
+}
+console.log(total);
+
+//Q2: 1001.8660608000002
+const calcBalance = (years, start) => {
+  let balance = start;
+  for (let i = 1; i <= years; i++) {
+    balance += i % 2 !== 0 ? balance * 0.2 : balance * 0.1;
+  }
+  return balance;
+};
+console.log(calcBalance(2, 100));
+
+//Q3: [ 1, 2, 3, 8 ]
+let greater = (it) => it * 2;
+console.log([0, 1, 2, 3, 0, 8].filter(greater));
+
+//Q4: https://unix.stackexchange.com/questions/656248/what-does-sudo-rm-rf-do
